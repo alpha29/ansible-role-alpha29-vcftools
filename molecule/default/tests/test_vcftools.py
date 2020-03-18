4cab83a738ec64aba7c0d67af5ac117b5baceb13
vanguard737 <vanguard737@gmail.com>
2020-03-18 09:14:09 -0500

    Fixes test.

--- molecule/default/tests/test_vcftools.py
+++ molecule/default/tests/test_vcftools.py
@@ -5,4 +5,4 @@ def test_vcftools(host):
     cmd = "vcftools --version"
     cmd_result = host.run(cmd)
     assert cmd_result.rc == 0, "'{}' returned status {}.".format(cmd, cmd_result.rc)
-    assert "VCFtools (0.1.16000)" in cmd_result.stdout, f"'{cmd}' returned stdout '{cmd_result.stdout}', stderr '{cmd_result.stderr}'"
+    assert "VCFtools (0.1.16)" in cmd_result.stdout, f"'{cmd}' returned stdout '{cmd_result.stdout}', stderr '{cmd_result.stderr}'"
